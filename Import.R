5b50e5f0e8d59d91e70f7d19d0c3bcd9285764ac
theresamay <theresa.may.001@gmail.com>
2016-07-26 17:29:41 -0700

    converted spaces to NA cleaned up comments

--- Import.R
+++ Import.R
@@ -2,8 +2,13 @@
 ### Importing and cleansing DW data
 ###########################################################
 
-# assemble the main df
+# assemble the main datafile
 DWData <- read.csv("DW_Data/DW_Projects.csv", header=TRUE, sep = ",", stringsAsFactors = TRUE,
                     as.is = c("Summary"))
 rownames(DWData) <- DWData$Key
+# end
 
+# Convert empty values to NA
+DWData[DWData == ""] <- NA
+#DWData[DWData == " "] <- NA # there are still some rows ending with an extra space
+# end
