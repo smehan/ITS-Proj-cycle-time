a945c582292937138d29481f2fd7f843e8fa8089
theresamay <theresa.may.001@gmail.com>
2016-07-26 17:50:09 -0700

    Don't need XML data structures.

--- Import.R
+++ Import.R
@@ -1,6 +1,14 @@
 ###########################################################
 ### Importing and cleansing DW data
 ###########################################################
+library(ggplot2)
+library(scales)
+library(ggthemes)
+library(plyr)
+library(dplyr)
+library(stringr)
+library(reshape2)
+library(lubridate)
 
 # assemble the main datafile
 DWData <- read.csv("DW_Data/DW_Projects.csv", header=TRUE, sep = ",", stringsAsFactors = TRUE,
@@ -12,3 +20,8 @@ rownames(DWData) <- DWData$Key
 DWData[DWData == ""] <- NA
 #DWData[DWData == " "] <- NA # there are still some rows ending with an extra space
 # end
+
+# Convert dates to posix date objects
+DWData$Created <- mdy(DWData$Created)
+DWData$Resolved <- mdy(DWData$Resolved)
+DWData$Due.Date <- mdy(DWData$Due.Date)
