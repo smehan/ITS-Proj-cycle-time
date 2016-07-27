863da8219521a314d3ddd60aeae1323dbb89685d
theresamay <theresa.may.001@gmail.com>
2016-07-26 17:24:46 -0700

    Read in DW_Projects file - JIRA data for data warehouse projects

--- Import.R
+++ Import.R
@@ -1,3 +1,9 @@
 ###########################################################
 ### Importing and cleansing DW data
 ###########################################################
+
+# assemble the main df
+DWData <- read.csv("DW_Data/DW_Projects.csv", header=TRUE, sep = ",", stringsAsFactors = TRUE,
+                    as.is = c("Summary"))
+rownames(DWData) <- DWData$Key
+
