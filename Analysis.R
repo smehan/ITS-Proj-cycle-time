75c3e26885f31201c0a58f508faa565bb53badfd
theresamay <theresa.may.001@gmail.com>
2016-08-20 22:30:15 -0700

    added various models for experimentation

--- Analysis.R
+++ Analysis.R
@@ -7,18 +7,35 @@ library(dplyr)
 library(qcc)
 
 # Load in the project duration data from disk
-PDuration <- readRDS(file="DW_data/Project_Duration.rds")
+PDuration_P <- readRDS(file="DW_data/Project_Duration_Post.rds")
 cycles2 <- readRDS(file="DW_data/Cycles2.rds")
-cycles3 <- readRDS(file="DW_data/Cycles3.rds")
+cycles_P2 <- readRDS(file="DW_data/Cycles_P2.rds")
 # end
 
 ################################################################
 #  create linear models
 ################################################################
-mod1 <- lm(prc ~ mean_proj_duration + total_proj_duration + tot_projects,
-       data=cycles3)
+
+mod1 <- lm(mean_proj_duration ~ seq_along(QTR), data=cycles2)
 summary(mod1)
 
-mod2 <- lm(prc ~ mean_proj_duration + total_proj_duration ,
-           data=cycles3)
+mod2 <- lm(mean_proj_duration ~ seq_along(QTR), data=cycles_P2)
 summary(mod2)
+
+mod3 <- lm(prc ~ seq_along(QTR), data=cycles2)
+summary(mod3)
+
+mod4 <- lm(prc ~ seq_along(QTR), data=cycles_P2)
+summary(mod4)
+
+mod5 <- lm(prc ~ tot_projects, data=cycles2)
+summary(mod5)
+
+mod6 <- lm(prc ~ tot_projects, data=cycles_P2)
+summary(mod6)
+
+mod7 <- lm(prc ~ seq_along(QTR) + mean_proj_duration + tot_projects, data=cycles2)
+summary(mod7)
+
+mod8 <- lm(prc ~ seq_along(QTR)+ mean_proj_duration + tot_projects, data=cycles_P2)
+summary(mod8)
