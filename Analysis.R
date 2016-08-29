7be7f4f52697cc988af3b93dd09e6817a15199c8
theresamay <theresa.may.001@gmail.com>
2016-08-29 13:24:46 -0700

    created pre-analysis plots.  removed reference to post data sets.  created the IMR Chart to get the VOP.  Created the Process Capability Chart to get the VOC.

--- Analysis.R
+++ Analysis.R
@@ -8,17 +8,26 @@ library(qcc)
 
 # Load in the project duration data from disk
 PDuration_P <- readRDS(file="DW_data/Project_Duration_Post.rds")
-cycles2 <- readRDS(file="DW_data/Cycles2.rds")
-cycles_P2 <- readRDS(file="DW_data/Cycles_P2.rds")
+cycles3_ed <- readRDS(file="DW_data/Cycles3_ed.rds")
 # end
+
+#####################################################################
+# Using qcc to get a simple Individuals Chart (IMR)
+# Get the voice of the process (VOP)
+nq <- qcc(cycles3_ed$prc, type="xbar.one", nsigmas=3)
+
+# Create a process capability analysis of the xbar.one
+# Set the upper, lower, and target specifications based on VOC
+process.capability(nq, spec.limits = c(25,75), target = 50)
+#####################################################################
 
 ################################################################
 #  create linear models
 ################################################################
-mod1 <- lm(mean_proj_duration ~ seq_along(QTR), data=cycles2)
+mod1 <- lm(prc ~ mean_proj_duration, data=cycles2)
 summary(mod1)
 
-mod2 <- lm(mean_proj_duration ~ seq_along(QTR), data=cycles_P2)
+mod2 <- lm(prc ~ mean_proj_duration, data=cycles_P2)
 summary(mod2)
 
 mod3 <- lm(prc ~ seq_along(QTR), data=cycles2)
@@ -33,23 +42,33 @@ summary(mod5)
 mod6 <- lm(prc ~ tot_projects, data=cycles_P2)
 summary(mod6)
 
-mod7 <- lm(prc ~ seq_along(QTR) + mean_proj_duration + tot_projects, data=cycles2)
+mod7 <- lm(prc ~ seq_along(QTR)+ mean_proj_duration + tot_projects, data=cycles2)
 summary(mod7)
 
-mod8 <- lm(prc ~ seq_along(QTR)+ mean_proj_duration + tot_projects, data=cycles_P2)
+mod8 <- lm(prc ~ seq_along(QTR) + mean_proj_duration + tot_projects, data=cycles_P2)
 summary(mod8)
 #####################################################################
 
-#####################################################################
-# Using qcc to get a simple Individuals Chart
-# Get the vector of observations in which are interested
 
-# Create an ImR object for process capability calculation.
-nq <- qcc(cycles2$prc, type="xbar.one", nsigmas=3)
+#########################################################
+### 2-way anova
+########################################################
 
-# Create an ImR object for process capability calculation.
-nq <- qcc(cycles_P2$prc, type="xbar.one", nsigmas=3)
-#####################################################################
+model1a <- lm(prc ~ mean_proj_duration + total_proj_duration + tot_projects + 
+                mean_proj_duration*total_proj_duration + mean_proj_duration*tot_projects + 
+                total_proj_duration*tot_projects,
+              cycles2)
+
+
+anova(model1a)
+
+
+
+
+
+
+
+
 
 ##########################################################
 # Multiple Regression Model 1 with all predictor variables
