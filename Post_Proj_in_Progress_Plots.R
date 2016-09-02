15a87347d031e765b31534a93838b066075a71eb
theresamay <theresa.may.001@gmail.com>
2016-09-01 20:54:37 -0700

    read in both the post files and updated titles as appropriate.

--- Post_Proj_in_Progress_Plots.R
+++ Post_Proj_in_Progress_Plots.R
@@ -1,5 +1,5 @@
 ###########################################################
-## Create general plots to understand DW project 
+## POST - Create general POST plots to understand DW project 
 ## coverage - cycles edited to show # projects
 ## in progress during each cycle (not only started)
 ###########################################################
@@ -9,33 +9,34 @@ library(dplyr)
 library(qcc)
 
 # Load in the project duration data from disk
-cycles3_ed <- readRDS(file="DW_data/Cycles3_ed.rds")
+PDuration_P <- readRDS(file="DW_data/Project_Duration_Post.rds")
+cycles3P_ed <- readRDS(file="DW_data/Cycles3P_ed.rds")
 # end
 
 ###########################################################
-## Create scatter plots of cycles3 edited data -
+## Create scatter plots of cycles3P edited data -
 ## Projects in progress
 ###########################################################
 
-ggplot(cycles3_ed) +
+ggplot(cycles3P_ed) +
   aes(x=seq_along(QTR), y=prc) +
   geom_point() +
   geom_smooth(method = lm) +
-  ggtitle("Percentage of Coverage by Cycle - Projects In-Progress") +
+  ggtitle("POST - Percentage of Coverage by Cycle - Projects In-Progress") +
   labs(x="Quarter", y="% of Coverage")
 
-ggplot(cycles3_ed) +
+ggplot(cycles3P_ed) +
   aes(x=tot_projects, y=prc) +
   geom_point() +
   geom_smooth(method = lm) +
-  ggtitle("% of Coverage by Total Projects In-Progress") +
+  ggtitle("POST - % of Coverage by Total Projects In-Progress") +
   labs(x="Total Projects In-Progress", y="% of Coverage")
 
-ggplot(cycles3_ed) +
+ggplot(cycles3P_ed) +
   aes(x=seq_along(QTR), y=prc) +
   geom_point(aes(size = total_proj_duration,
                  color = mean_proj_duration)) +
   geom_jitter() +
-  ggtitle("Coverage by Cycle - Projects In-Progress") +
+  ggtitle("POST - Coverage by Cycle - Projects In-Progress") +
   labs(x="Quarter", y="Coverage")
 
