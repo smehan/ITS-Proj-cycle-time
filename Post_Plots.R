0ce6f7368ee086f0feac23b8cda6a9605df293cd
theresamay <theresa.may.001@gmail.com>
2016-09-01 20:56:55 -0700

    set up with post files.  removed all related to cycles_P2.  Created the IMR and process capability charts.

--- Post_Plots.R
+++ Post_Plots.R
@@ -1,77 +1,31 @@
 ###########################################################
-## Create general plots to understand DW project
-## coverage - POST
+## Perform general analysis to understand DW project 
+## duration data
 ###########################################################
-library(ggplot2)
 library(reshape2)
 library(dplyr)
 library(qcc)
-# Load in the project duration data from disk
-PDuration_P <- readRDS(file="DW_data/Project_Duration_Post.rds")
-cycles_P2 <- readRDS(file="DW_data/Cycles_P2.rds")
-# end
 
-###########################################################
-## Create scatter plots of cycles_P2 data
-###########################################################
+# Load in the project duration data from disk
+PDuration_P <- read.csv("DW_Data/Project_Duration_Post", header=TRUE, sep = ",", stringsAsFactors = TRUE,
+                      as.is = c("Summary"))
+cycles3P_ed <- readRDS(file="DW_data/cycles3P_ed.rds")
+# end
 
 ggplot(PDuration_P) +
   aes(x=QTR, y=project_duration) +
   geom_point(aes(color = Dept)) +
   geom_jitter() +
-  ggtitle("Data Warehouse Projects - POST") +
+  ggtitle("POST - Data Warehouse Projects") +
   labs(x="Quarter", y="Project Duration")
 
-ggplot(cycles_P2) +
-  aes(x=seq_along(QTR), y=prc) +
-  geom_point() +
-  geom_smooth(method = lm) +
-  ggtitle("Percentage of Coverage by Cycle") +
-  labs(x="Quarter", y="% of Coverage")
+#####################################################################
+# Using qcc to get a simple Individuals Chart (IMR)
+# Get the voice of the process (VOP)
+nqP <- qcc(cycles3P_ed$prc, type="xbar.one", limits = c(22.91,50.29), nsigmas=3, title = "POST - Individuals Chart - % Coverage")
 
-ggplot(cycles_P2) +
-  aes(x=tot_projects, y=prc) +
-  geom_point() +
-  geom_smooth(method = lm) +
-  ggtitle("% of Coverage by Total Projects Started - POST") +
-  labs(x="Total Projects Started", y="% of Coverage")
-  
-###########################################################
-## Experiment with creating scatter plots of cycles_P2 data
-###########################################################
-ggplot(cycles_P2) +
-  aes(x=QTR, y=prc) +
-  geom_point() +
-  ggtitle("Percentage of Coverage - POST") +
-  labs(x="Quarter", y="Percentage")
-
-ggplot(cycles_P2) +
-  aes(x=QTR, y=tot_projects) +
-  geom_point(aes(size = total_proj_duration,color = prc)) +
-  geom_jitter() +
-  ggtitle("Total Projects by Cycle - POST") +
-  labs(x="Quarter", y="Total # of Projects")
-
-ggplot(cycles_P2) +
-  aes(x=QTR, y=prc) +
-  geom_point(aes(size = total_proj_duration,
-  color = mean_proj_duration)) +
-  geom_jitter() +
-  ggtitle("Coverage by Cycle - POST") +
-  labs(x="Quarter", y="Coverage")
-
-ggplot(cycles_P2) +
-  aes(x = QTR, y = total_proj_duration) +
-  geom_point(aes(color = prc)) +
-  scale_color_continuous_tableau()
-
-ggplot(cycles_P2) +
-  aes(x=mean_proj_duration, y=prc) +
-  geom_point(aes(color = tot_projects))
-
-ggplot(cycles_P2) +
-  aes(x=tot_projects, y=prc) +
-  geom_point(aes(color = mean_proj_duration)) +
-  scale_color_continuous_tableau()
-
+# Create a process capability analysis of the xbar.one
+# Set the upper, lower, and target specifications based on VOC
+process.capability(nqP, spec.limits = c(25,75), target = 50)
+#####################################################################
 
